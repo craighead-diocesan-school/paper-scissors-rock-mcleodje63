3dfa192ee97aa019031532214a8a641eab3e4510
mcleodje63 <mcleodje63@craighead.school.nz>
2023-06-13 09:57:27 +1200

    Created the repository
    Made a footer file, changed the intro paragraph, made a input textbox and a input button. Began the javascript section.

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,3 @@
+<footer>
+  <p>&copy; <a href="https://craighead.school.nz/"> Craighead Diocesan School 2023</a></p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,30 @@
 <script>
-  import Header from '$lib/Header.svelte'
+  import Header from "$lib/Header.svelte";
+  import Footer from "$lib/Footer.svelte";
+  let userMove;
+  function game() {
+    userMove = userMove.toLowerCase();
+    //   if (userMove == "paper") {
+    //   }
+    //   if (userMove == "rock") {
+    //   }
+    //   if (userMove == "scissors") {
+    //   }
+
+    //
+  }
 </script>
 
 <Header />
 
 <main>
-  <h2>SvelteKit</h2>
+  <h2>Welcome</h2>
 
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
+  <p>Paper Scissors Rock, also known as Rock Paper Scissors or simply RPS, is a popular hand game played between two individuals. The objective of the game is to select one of three possible hand gestures—rock, paper, or scissors—while simultaneously trying to predict the gesture chosen by the opponent. The game follows a set of predefined rules that determine the outcome based on the combination of gestures chosen by both players.</p>
+  <br />
+  Use the box below to play!
+  <br />
+  <input bind:value={userMove} /><button on:click={game}>Click here</button>
 </main>
 
-<footer>
-  <p>&copy; Craighead Diocesan School 2023</p>
-</footer>
+<Footer />
